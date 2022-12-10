110a891bd21b2191c0571e9fb19e72796d33f922
jy <wndud0249@naver.com>
2022-12-10 09:07:41 +0900

    initial commit

---
+++ back_end/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.listen(8080, function () {
+  console.log('listening on 8080');
+});
+
+app.get('/api/comics/romance', (req, res) => {
+  if (req.query.page < 6 && req.query.page > 0) {
+    const dataBuffer = fs.readFileSync(__dirname + `/page_${req.query.page}.json`);
+    const resJson = dataBuffer.toString();
+    res.status(200).send(resJson);
+  } else {
+    res.status(404).json({ code: 404, status: 'fail', success: false });
+  }
+});
